14a1b6414bcddbf9339ae8817c78aecf323f66c0
Igor <104066443+OldXrist@users.noreply.github.com>
2025-01-21 22:30:13 +0300

    Added rate_limit command + def for groq chat comp

--- handlers/groq.py
+++ handlers/groq.py
@@ -37,14 +37,48 @@ async def groq_answer_handler(message: types.Message) -> None:
     """
     Handler to send the user's question to OpenAI API and return the answer.
     """
-    try:
-        user_question = message.text
+    await groq_api_request(message.text, message)
 
-        # Call OpenAI API with updated method
+
+@router.message(F.voice, IsAdminFilter(ADMIN_IDS))
+async def handle_voice_message(message: types.Message):
+    """Handles voice messages, transcribes them, and sends the text to the user."""
+    user_id = message.from_user.id
+    voice: types.Voice = message.voice
+
+    try:
+        # Step 1: Download and convert the voice message to WAV
+        wav_file_path = await download_and_convert_voice(voice, user_id)
+
+        # Step 2: Transcribe the audio using OpenAI Whisper API
+        transcription = await transcribe_audio_with_groq(wav_file_path)
+
+        # Step 3: Reply with the transcription
+        await groq_api_request(transcription, message)
+
+    except Exception as e:
+        logger.error(f'Speech recognition error: {e}')
+        await message.answer(f"Извините, что-то пошло не так при обработке вашего голосового сообщения.")
+
+
+@router.message(IsAdminFilter(ADMIN_IDS))
+async def other_content_handler(message: types.Message) -> None:
+    await message.answer('К сожалению, я могу работать только с текстовым и голосовым контеном.'
+                         '\n\nС чем еще я могу вам помочь?')
+
+
+@router.message()
+async def unauthorized_message_handler(message: types.Message) -> None:
+    await message.answer('Ваш запрос не может быть обработан, поскольку вы не авторизованы.')
+
+
+async def groq_api_request(prompt: str, message: types.Message) -> None:
+    try:
+        # Call Groq API with updated method
         response = client.chat.completions.create(
             messages=[
                 {"role": "system", "content": CONTEXT},
-                {"role": "user", "content": user_question},
+                {"role": "user", "content": prompt},
             ],
             model="llama-3.3-70b-versatile"
         )
@@ -62,35 +96,3 @@ async def groq_answer_handler(message: types.Message) -> None:
     except Exception as e:
         logger.error(f"Groq API error: {e}")
         await message.answer("Извините, я не смогла обработать ваш запрос. Пожалуйста, попробуйте снова позже.")
-
-
-@router.message(F.voice, IsAdminFilter(ADMIN_IDS))
-async def handle_voice_message(message: types.Message):
-    """Handles voice messages, transcribes them, and sends the text to the user."""
-    user_id = message.from_user.id
-    voice: types.Voice = message.voice
-
-    try:
-        # Step 1: Download and convert the voice message to WAV
-        wav_file_path = await download_and_convert_voice(voice, user_id)
-
-        # Step 2: Transcribe the audio using OpenAI Whisper API
-        transcription = await transcribe_audio_with_groq(wav_file_path)
-
-        # Step 3: Reply with the transcription
-        await message.reply(f"Вот текст, который мне удалось распознать:\n\n{transcription}\n\n"
-                            f"Обратите внимание: модель может допускать ошибки в распознавании. "
-                            f"Пожалуйста, проверьте текст перед использованием. "
-                            f"Если что-то не так, вы всегда можете отправить сообщение повторно.")
-    except Exception as e:
-        await message.reply(f"Извините, что-то пошло не так при обработке вашего голосового сообщения.")
-
-
-@router.message(IsAdminFilter(ADMIN_IDS))
-async def other_content_handler(message: types.Message) -> None:
-    await message.answer('К сожалению, я могу работать только с текстовым контентом.\n\nС чем еще я могу вам помочь?')
-
-
-@router.message()
-async def unauthorized_message_handler(message: types.Message) -> None:
-    await message.answer('Ваш запрос не может быть обработан, поскольку вы не авторизованы.')
